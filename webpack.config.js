086b782b59526bd1872d6f5b64055ee01ce38687
권묭히 <gwonmyonghi@gwonmyonghiui-MacBookPro.local>
2020-04-08 19:03:03 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: ['@babel/polyfill', './src/js/main.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src/js')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      filename: path.join(__dirname, './dist/index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  devtool: 'source-map',
+  mode: 'development',
+};
