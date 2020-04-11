7f7654decc6045300bdd64a85eaa932afdcc074d
maymkwon <may.m.kwon@gmail.com>
2020-04-11 11:44:09 +0900

    controller

---
+++ src/js/scripts/controller.js
@@ -0,0 +1,10 @@
+export default class Controller {
+  constructor(model, view){
+    this.model = model;
+    this.view = view
+
+
+  }
+}
+
+https://github.com/JaeYeopHan/ecma2015_TodoMVC/blob/master/js/controller.js
